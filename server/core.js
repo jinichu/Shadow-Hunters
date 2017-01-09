ed8d39227ede460c46338b14d69faee71b4f3845
Jinny Byun <jinnybyun@gmail.com>
2017-01-08 21:44:02 -0800

    Added ability to pick up cards from corresponding location on board

--- server/core.js
+++ server/core.js
@@ -4,90 +4,6 @@ if (!window.RTCPeerConnection) {
 
 const COLORS = ["black", "purple", "red", "yellow", "blue", "green", "pink", "orange"];
 
-const CHARACTERS = {
-  "Catherine": {
-    health: 11,
-    faction: "Neutral",
-    winCondition: (player, state) => {
-      let othersDead = false;
-      for (let p of state.players) {
-        if (player !== p && p.dead) {
-          othersDead = true;
-          break;
-        }
-      }
-      return player.dead && !othersDead;
-    },
-    image: "client/catherine.png",
-    winConditionDescription: "Be the first one to die",
-    abilityDescription: "(Survival) Heal one point of damage per turn"
-  },
-  "Bob": {
-    health: 11,
-    faction: "Neutral",
-    winCondition: (player, state) => {
-    },
-    image: "client/bob.png",
-    winConditionDescription: "Collect five items",
-    abilityDescription: "(Get Money) Instead of doing damage, you can choose to steal items"
-  },
-  "Vampire": {
-    health: 13,
-    faction: "Shadow",
-    winCondition: (player, state) => {
-      let shadowsWin = false;
-      for (let player of state.players){
-        if (CHARACTERS[player.name].faction === "Hunter"){
-          if (!player.dead){
-            shadowsWin = false;
-          }
-          else {
-            shadowsWin = true;
-          }
-        }
-      }
-      return shadowsWin;
-    },
-    image: "client/vampire.png",
-    winConditionDescription: "Kill all the hunters",
-    abilityDescription: "(Lifesteal) Heal two points of damage after attack succeeds"
-  },
-  "George": {
-    health: 14,
-    faction: "Hunter",
-    winCondition: (player, state) => {
-      let huntersWin = false;
-      for (let player of state.players){
-        if (CHARACTERS[player.name].faction === "Shadow"){
-          if (!player.dead){
-            huntersWin = false;
-          }
-          else {
-            huntersWin = true;
-          }
-        }
-      }
-      return huntersWin;
-    },
-    image: "client/george.png",
-    winConditionDescription: "Kill all the shadows",
-    abilityDescription: "(Demolish) Choose someone to inflict four points of damage to"
-  }
-};
-
-const GREENCARDS = [
-  {
-    name: "Exorcism",
-    type: null,
-    effect: "Shadows take 2 damage"
-  },
-  {
-    name: "Slap",
-    type: null,
-    effect: "Hunters take 1 damage"
-  }
-];
-
 const WEBRTC_CONFIG = {
   iceServers: [
     // STUN servers
@@ -100,6 +16,7 @@ const WEBRTC_CONFIG = {
     },
   ]
 };
+
 const dataChannelOptions = {
   ordered: true
 };
@@ -295,11 +212,6 @@ class Server {
         }
       } else if (data.op === "reveal") {
         this.currentPlayer().isRevealed = true;
-        this.broadcastState();
-      } else if (data.op === "greenCard"){
-        const i = Math.floor(Math.random()*2);
-        this.state.card.name = GREENCARDS[i].name;
-        this.state.card.effect = GREENCARDS[i].effect;
         this.broadcastState();
       } else if (data.op === "attack") {
         const p = this.currentPlayer(data.player);
